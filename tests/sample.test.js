6d19d7206ef0055a1700ad796721a819981da10a
Pavan Chilukuri <ichilupa@gmail.com>
2018-02-06 19:38:40 -0600

    uploaded from local to remote

---
+++ tests/sample.test.js
@@ -0,0 +1,5 @@
+describe('Sample test description', function () { // description to the test 
+    it('Testing Strings', function () { // it gives name to the test
+        expect('XYZ').toBe('XYZ');
+    });
+});
